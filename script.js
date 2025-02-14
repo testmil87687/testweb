ae6a9abbef4fd2466e205acabd8d6c550b85132a
testmil87687 <t07023944@gmail.com>
2025-02-14 01:27:34 -0800

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("contactForm").addEventListener("submit", function(event) {
-    event.preventDefault();
-    alert("Thank you for contacting us! We will get back to you soon.");
-    this.reset();
-});
